57cdaa80882dd2b1da680a00a16f26e66a2f26c7
nika <nika.gogua8@gmail.com>
2023-01-03 18:33:18 +0400

    Markup for Mobile Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-grayish": "hsl(270, 3%, 87%)",
+        "dark-grayish ": "hsl(279, 6%, 55%)",
+        "dark-violet": "hsl(278, 68%, 11%)",
+        "gradient-1": "hsl(249, 99%, 64%)",
+        "gradient-2": "hsl(278, 94%, 30%)",
+      },
+      backgroundImage: {
+        "main-mob": "url('../images/bg-main-mobile.png')",
+      },
+    },
   },
   plugins: [],
 };
